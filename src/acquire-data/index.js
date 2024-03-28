ff744418b462b35c65e8cc586d691ffcccc68833
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-28 10:41:27 +0000

    remove `for` loop

--- src/acquire-data/index.js
+++ src/acquire-data/index.js
@@ -1,17 +1,11 @@
 export function acquireData(input) {
   const lines = input.split('\n');
-  let result = [];
-  const loopLines = lines
+  let result = lines
     .slice(1)
     .filter(line => line.trim() !== '')
     .map(line => line.split(','))
     .filter(record => record[1].trim() === 'India')
     .map(record => ({ city: record[0].trim(), phone: record[2].trim() }));
-
-  for (const line of loopLines) {
-    const record = line;
-    result.push(line);
-  }
 
   return result;
 }
